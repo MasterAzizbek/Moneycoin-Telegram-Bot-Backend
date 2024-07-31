de1d49dcad94ecc024a05b0a1370c0eb7ce7224a
Azizbek <alievazizbek93@gmail.com>
2024-07-31 18:45:08 +0500

    Updated

--- blum_bot/settings.py
+++ blum_bot/settings.py
@@ -12,16 +12,17 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4h=oes943lr@(st63mgiu$bk*7&_*^*(e@geipna7wc94@d&ha'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
